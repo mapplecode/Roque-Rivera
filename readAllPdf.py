1a6b33d5e25c15d1090a64e52d67a2fd966ae9e1
mapplecode2020 <mapplecode2020@gmail.com>
2022-11-07 13:13:48 +0530

    few changes in filter data

--- readAllPdf.py
+++ readAllPdf.py
@@ -1,15 +1,22 @@
 import glob,json
 import pprint
 import PyPDF2,os
-
+import re
+def partial(lst, query):
+    pattern = '.*' + query + '.*'
+    return [x for x in lst if re.match(pattern, x)]
 filePath = os.getcwd()
 newpath = filePath +"/"+"Json File"
-os.makedirs(newpath)
-if not os.path.exists(filePath):
-    os.makedirs(newpath)
+
+try:
+    print(os.path.exists(os.path.join(os.getcwd(),'Json File')))
+    if not os.path.exists(os.path.join(os.getcwd(),'Json File')):
+        os.makedirs(os.path.join(os.getcwd(),'Json File'))
+except Exception as e:
+    print(e)
 mylist = [file for file in glob.glob("StoreFile/*.pdf")]
 data = [PyPDF2.PdfFileReader(getListOfFile) for getListOfFile in mylist]
-matchWord = ["Packing Slip","INVOICE","PUBLIC TELEPHONE COMPANY","Purchase Order","Receipt","invoice number"]
+matchWord = ["Packing Slip","invoice","Purchase Order","Receipt","invoice number"]
 
 def FileNames():
 	store = [getFileName.split("StoreFile\\")[1] for getFileName in mylist]
@@ -19,15 +26,30 @@ def FileNames():
 iterData = iter(FileNames())
 def readData():
     AlljsonHere = list()
-    for reader in data: 
+    for reader in data:
         page = reader.pages
         for pages in page:
-            text = pages.extract_text().lower()
-            convertIntoList = text.split('\n')
-            for matchListKeyword in matchWord:
-                if matchListKeyword.lower() in convertIntoList:
-                    flName = next(iterData)
-                    AlljsonHere.append({"FileName":flName,"Pdf Type":matchListKeyword,"Data":convertIntoList})
+            try:
+                text = pages.extract_text().lower()
+                print(text)
+                convertIntoList = text.split('\n')
+
+                convertIntoList_filtrd = []
+                convertIntoList_filtrd_real = []
+                for txtt in convertIntoList:
+                    convertIntoList_filtrd.append(txtt.replace('\xa0',' ').lower().strip())
+                    convertIntoList_filtrd_real.append(txtt.replace('\xa0', ' ').strip())
+                print(convertIntoList_filtrd)
+                Others = "Others"
+                flName = next(iterData)
+                for matchListKeyword in matchWord:
+                    if partial(convertIntoList_filtrd , matchListKeyword):
+                        Others = matchListKeyword
+                AlljsonHere.append(
+                    {"FileName": flName, "Pdf Type": Others, "Data": convertIntoList_filtrd_real})
+
+            except Exception as e:
+                print(e)
     return AlljsonHere
 
 datas = readData()
@@ -35,8 +57,11 @@ for i in datas:
     GetFileName = i.get("FileName").lower()
     convertFileType = GetFileName.replace("pdf","json")
     ConvertIntoString = json.dumps(i)
-    with open(f"Json File/ {convertFileType}","w+") as file:
-        store = file.write(ConvertIntoString)
+    try:
+        with open(f"Json File/ {convertFileType}","w+") as file:
+            store = file.write(ConvertIntoString)
+    except Exception as e:
+        print(e)
 
 
  
